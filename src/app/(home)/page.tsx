11362ae128f221f8b6921dbee07a7298172284b3
Samuel-3004 <samuelszportela@hotmail.com>
2023-10-19 16:43:46 -0300

    feat: add keyboards section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductList from "./components/product-list";
+import SectionTitle from "./components/section-title";
+import PromoBanner from "./components/promo-banner";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -12,31 +14,57 @@ export default async function Home() {
     },
   });
 
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+    },
+  });
+
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
+
   return (
     <div>
-      <Image
+      <PromoBanner
         src="/banner-home-01.png"
-        height={0}
-        width={0}
-        className="h-auto w-full px-5"
-        sizes="100vw"
         alt="Até 55% de desconto esse mês."
-      ></Image>
+      />
       <div className="mt-8 px-5">
         <Categories />
       </div>
       <div className="mt-8">
-        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
+        <SectionTitle className="mb-3 pl-5 font-bold uppercase">
+          Ofertas
+        </SectionTitle>
         <ProductList products={deals} />
       </div>
-      <Image
+      <PromoBanner
         src="/banner-home-02.png"
-        height={0}
-        width={0}
-        className="h-auto w-full px-5"
-        sizes="100vw"
         alt="Até 55% de desconto em mouses."
-      ></Image>
+      />
+      <div className="mt-8">
+        <SectionTitle className="mb-3 pl-5 font-bold uppercase">
+          Teclados
+        </SectionTitle>
+        <ProductList products={keyboards} />
+      </div>
+      <PromoBanner
+        src="/banner-home-03.png"
+        alt="Até 20% de desconto em fones."
+      />
+      <div className="mt-8">
+        <SectionTitle className="mb-3 pl-5 font-bold uppercase">
+          Mouses
+        </SectionTitle>
+        <ProductList products={mouses} />
+      </div>
     </div>
   );
 }
