94e3f35cac61de024dfbfd14a067fb22115b12c5
Samuel-3004 <samuelszportela@hotmail.com>
2023-10-19 23:02:32 -0300

    feat: add third promo banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -31,40 +31,39 @@ export default async function Home() {
   });
 
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <PromoBanner
         src="/banner-home-01.png"
         alt="Até 55% de desconto esse mês."
       />
-      <div className="mt-8 px-5">
+
+      <div className="px-5">
         <Categories />
       </div>
-      <div className="mt-8">
-        <SectionTitle className="mb-3 pl-5 font-bold uppercase">
-          Ofertas
-        </SectionTitle>
+
+      <div>
+        <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
+
       <PromoBanner
         src="/banner-home-02.png"
         alt="Até 55% de desconto em mouses."
       />
-      <div className="mt-8">
-        <SectionTitle className="mb-3 pl-5 font-bold uppercase">
-          Teclados
-        </SectionTitle>
+
+      <div>
+        <SectionTitle>Teclados</SectionTitle>
         <ProductList products={keyboards} />
       </div>
-      <PromoBanner
-        src="/banner-home-03.png"
-        alt="Até 20% de desconto em fones."
-      />
-      <div className="mt-8">
-        <SectionTitle className="mb-3 pl-5 font-bold uppercase">
-          Mouses
-        </SectionTitle>
-        <ProductList products={mouses} />
+
+      <div>
+        <PromoBanner
+          src="/banner-home-03.png"
+          alt="Até 20% de desconto em fones."
+        />
       </div>
+
+
     </div>
   );
 }
