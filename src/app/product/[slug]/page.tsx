42d3d8e07d28d767245a3da608bf7da2e53994c4
Samuel-3004 <samuelszportela@hotmail.com>
2023-10-21 13:17:31 -0300

    feat: ensure recommended products are show on product details page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { prismaClient } from "@/lib/prisma";
 import ProductImages from "./components/product-images";
 import ProductInfo from "./components/products-info";
 import { computeProductTotalPrice } from "@/helpers/product";
+import ProductList from "@/components/ui/product-list";
 
 interface IProductDeteilsPageProps {
   params: {
@@ -16,16 +17,31 @@ const ProductDetailsPage = async ({
     where: {
       slug: slug,
     },
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              slug: {
+                not: slug,
+              },
+            },
+          },
+        },
+      },
+    },
   });
+
   if (!product) return null;
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 pb-8">
       <ProductImages imagesUrls={product.imageUrls} name={product.name} />
       <ProductInfo
         product={computeProductTotalPrice(product)}
         key={product.id}
       />
+      <ProductList products={product.category.products} />
     </div>
   );
 };
