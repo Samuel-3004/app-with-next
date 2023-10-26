8ee61124d8927bca2cbd30d6d4c3c3a53e299ce0
Samuel-3004 <samuelszportela@hotmail.com>
2023-10-26 15:21:08 -0300

    chore: installing dependencies and changing component directory

---
+++ src/components/ui/product-list.tsx
@@ -0,0 +1,24 @@
+import ProductItem from "@/components/ui/product-item";
+import { computeProductTotalPrice } from "@/helpers/product";
+import { Product } from "@prisma/client";
+
+interface IProductListProps {
+  products: Product[];
+}
+
+const ProductList = ({ products }: IProductListProps) => {
+  return (
+    <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+      {products.map((product) => (
+        <div key={product.id} className="w-[170px] max-w-[170px]">
+          <ProductItem
+            product={computeProductTotalPrice(product)}
+            className="w-[156px]"
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
